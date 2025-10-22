4cb90d80029c8d3b0fc3cceef0427649bd155ebe
AlbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-10-22 21:25:21 +0200

    added linkedin card in conctact page

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -51,6 +51,7 @@ const Contact: React.FC = () => {
             ),
             action: "tel:+34667381410",
         },
+
         {
             id: "github",
             label: "GitHub",
@@ -82,6 +83,22 @@ const Contact: React.FC = () => {
                 </svg>
             ),
             action: "https://www.infojobs.net/candidate/cv/view/index.xhtml  ",
+        },
+        {
+            id: "linkedin",
+            label: "LinkedIn",
+            value: "https://www.infojobs.net/candidate/cv/view/index.xhtml  ",
+            icon: (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                >
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
+                </svg>
+            ),
+            action: "https://www.linkedin.com/in/alberto-andrade-216602318/",
         },
     ];
 
