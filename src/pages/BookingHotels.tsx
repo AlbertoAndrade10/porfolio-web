a4bf01dca78605187555e8fc6500d738092a2594
Alberto Andrade Ortiz <albertoandradeortiz910@gmail.com>
2026-01-11 12:37:09 +0100

    BookingHotels page uploaded

--- src/pages/BookingHotels.tsx
+++ src/pages/BookingHotels.tsx
@@ -9,9 +9,9 @@ const BookingHotels: React.FC = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 013-3h6a3 3 0 013 3v6a3 3 0 01-3 3z" />
                 </svg>
             ),
-            title: "Autenticación",
+            title: "Arquitectura de microservicios",
             description:
-                "Registro y login de usuarios",
+                "Un proyecto desarrollado con arquitectura de microservicios usando NestJS y Spring Boot como tecnologías principales.",
         },
         {
             icon: (
@@ -19,9 +19,9 @@ const BookingHotels: React.FC = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 10a3 3 0 11-6 0 3 3 0 016 0zm6-3a6 6 0 01-6 6H6a6 6 0 01-6-6v-2c0-1.1.9-2 2-2h12a2 2 0 012 2v2z" />
                 </svg>
             ),
-            title: "Catálogo",
+            title: "Autenticación",
             description:
-                "Elije la habitación que más te guste en nuestro sistema, realiza la reserva para el tiempo que desees y recibe toda la información por correo electrónico.",
+                "Login de usuario, Registro de usuario, creación de perfil y administración del mismo protegidos por roles y JWT",
         },
         {
             icon: (
@@ -29,9 +29,29 @@ const BookingHotels: React.FC = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                 </svg>
             ),
-            title: "Valoraciones",
+            title: "Hoteles",
             description:
-                "Deja un comentario y valora tu experiencia.",
+                "Lista de hoteles con sus diferentes tipos de habitaciones como standard, deluxe o suite.",
+        },
+        {
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
+            ),
+            title: "Control de disponibilidad",
+            description:
+                "Se valida si la habitación está disponible previo a la reserva",
+        },
+        {
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
+            ),
+            title: "Otros",
+            description:
+                "Microservicios protegidos por API Gateway y JWT.",
         },
     ];
 
